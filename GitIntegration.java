7a2ad9c0889e5391c2065c6107991a1dba92db1f
Kanchan Jahagirdar <Kanchan.Jahagirdar@sqs.com>
2018-11-27 15:10:55 +0100

    first time

---
+++ GitIntegration.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package demo1;
+
+/**
+ * @author JahagirdarK
+ *
+ */
+public class GitIntegration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int i=0;
+		
+		// TODO Auto-generated method stub
+for (i=0;i<=5;i++) {
+		System.out.println("Hellow World");
+}
+	}
+
+}
